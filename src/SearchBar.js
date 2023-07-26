ec2a08a479e44d76cfcb94821e636e67175d8e37
mike <mjowiti@gmail.com>
2023-07-26 23:45:11 +0300

    final checks

--- src/SearchBar.js
+++ src/SearchBar.js
@@ -1,37 +1,38 @@
-import React from 'react';
-
-
-const SearchBar=({searchParameter,onTransSearch,setFilter,transaction})=>{
-    function handleTransSearch(e){
-        let search=e.target.value
-        onTransSearch(search)
-        console.log(search);
-
-        if(search !==""){
-        const filteredArray=[...transaction]
-       const filter= filteredArray.filter((item)=>item.description.toLowerCase() === search);
-       console.log(filter);
-       setFilter(filter)
-        }
-        else {
-            setFilter(transaction)
-        }
-       
-        
-
-    }
+import React, { useState } from "react";
+function SearchBar({search,filtered,setFiltered, setSearch,transactions,setTransactions}){
     
 
-    return(
-        <>
-        <input type="text"
-         name="searchbar"
-         value={searchParameter}
-         placeholder="search transaction"
-         onChange={handleTransSearch}
-        />
-         <i className="circular search link icon"></i>
-        </>
-    )
+    
+
+    function handleChange(search){
+        setSearch(search)
+        const updatedList=[...transactions]
+       
+    
+    //     const filteredTransactions=transactions.filter((transaction)=>transaction.category.toLowerCase() === search)
+    //     setFiltered(filteredTransactions)
+    //     console.log(filtered);
+    //
+    if(search !==""){
+        const filteredArray=[...transactions]
+       const filter= filteredArray.filter((transaction)=>transaction.description.toLowerCase() === search);
+       
+   console.log(filter);
+     setFiltered(filter)
+
+        }
+        else {
+            setFiltered(transactions)
+        }
+}
+    
+    
+    
+return (
+    <div>
+        <input name="searchbar" placeholder="search transaction" value={search} onChange={(e)=>handleChange(e.target.value)}/>
+        <i className="circular search link icon"></i>
+    </div>
+)
 }
 export default SearchBar;
--- src/Transactions.js
+++ src/Transactions.js
@@ -1,32 +1,74 @@
+import { useState,useEffect } from "react";
 import React from "react";
 
-const Transactions=({transaction,onDeleteTransaction})=>{
-    const  {date,description,category,amount}= transaction;
-
-    //delete transaction
-    function handleDeleteTransaction(){
-        fetch(`http://localhost:3000/transactions/${transaction.id}`,{
-            method: "DELETE",})
-            .then((res)=>res.json())
-            .then(()=>onDeleteTransaction(transaction))
-        
-    }
+function Transaction({search,filtered,transactions,setTransactions}){
 
 
+function table(transaction){
+  return(
+    <tr key={transaction.id}>
+    <td>{transaction.date}</td>
+    <td>{transaction.description}</td>
+    <td>{transaction.category}</td>
+    <td>{transaction.amount}</td>
+    <button onClick={() =>handleDelete(transaction.id)}>delete</button>
+    </tr>
 
-return (
-    <tbody>
-    <tr>
-    <td>{date}</td>
-    <td>{description}</td>
-    <td>{category}</td>
-    <td>{amount}</td>
-    <td>
-    <button  style={{ color: "red" }}  onClick={handleDeleteTransaction}>Delete</button>
-    </td>
-  </tr>
-  </tbody>
+  )
+  }
+
+  const filteredTransaction=filtered.map((transaction)=>{
+  return table(transaction)})
+  console.log(filtered);
+
+  const nonFilteredTransaction=transactions.map((transaction)=>{
+    return table(transaction)})
+  
+  
+
+function handleDelete(id) {
+    fetch(`http://localhost:3000/transactions/${id}`, {
+      method: "DELETE",
+    })
+      .then((r) => r.json())
+      .then(() => {
+        const updatedTransactions = transactions.filter((transaction) => transaction.id !== id);
+        setTransactions(updatedTransactions);
+      });
+  } 
+
+return(
+<div>
+
+    <table>
+        <thead>
+        <tr>
+        <th>Date</th>
+        <th>Description</th>
+        <th>Category</th>
+        <th>Amount</th>
+        <th>Delete</th>
+
+        </tr>
+        </thead>
+        <tbody>
+          {search.length >0 ? filteredTransaction : nonFilteredTransaction}
+      
+            {/* {transactions.map((transaction)=>
+            <tr key={transaction.id}>
+            <td>{transaction.date}</td>
+            <td>{transaction.description}</td>
+            <td>{transaction.category}</td>
+            <td>{transaction.amount}</td>
+            <button onClick={() =>handleDelete(transaction.id)}>delete</button>
+            </tr>)} */}
+
+        </tbody>
+    </table>
+</div>
+
 )
-}
 
-export default Transactions;
+
+}
+export default Transaction;
